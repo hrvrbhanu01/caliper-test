be2d15ef5d8c9499acb596af3bbc0736ee892484
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2024-08-12 12:45:55 +0530

    read case updated

--- workload/read.js
+++ workload/read.js
@@ -5,7 +5,7 @@ const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
 class queryAppointmentWorkload extends WorkloadModuleBase {
     constructor() {
         super();
-        this.appointmentIds = [];
+        this.id = [];
         this.txIndex = 0;
     }
 
@@ -15,7 +15,7 @@ class queryAppointmentWorkload extends WorkloadModuleBase {
         // Generate a list of appointment IDs to read
         // This assumes you've already created appointments with IDs in this format
         for (let i = 1; i <= 1000; i++) {  // Adjust this number based on how many appointments you expect to have
-            this.appointmentIds.push(`app${i}`);
+            this.id.push(`app${i}`);
         }
     }
 
@@ -23,11 +23,11 @@ class queryAppointmentWorkload extends WorkloadModuleBase {
         this.txIndex++;
         
         // Randomly select an appointment ID to read
-        const appointmentId = this.appointmentIds[Math.floor(Math.random() * this.appointmentIds.length)];
+        const appointmentId = this.id[Math.floor(Math.random() * this.id.length)];
 
         const myArgs = {
             contractId: this.roundArguments.contractId,
-            contractFunction: 'queryAppointment',  // Assume you have a readAppointment function in your chaincode
+            contractFunction: 'getAppointment',  // Assume you have a readAppointment function in your chaincode
             contractArguments: [appointmentId],
             readOnly: true
         };
